1ef10a888060631c422ea9925b2817f286bf7d4a
Brandon Liu <brandon.slipjet@gmail.com>
2018-02-08 19:25:29 -0800

    partials ball/ paddle/ game almost complete

--- src/partials/Game.js
+++ src/partials/Game.js
@@ -1,6 +1,9 @@
 
 import { SVG_NS, KEYS } from '../settings';
 import Board from './Board';
+import Paddle from './Paddle';
+import Ball from './Ball';
+
 export default class Game {
 
 	constructor(element, width, height) {
@@ -14,14 +17,28 @@ export default class Game {
 		this.paddleWidth = 8;
 		this.paddleHeight = 56;
 		this.boardGap = 10;
-		
+
 		this.player1 = new Paddle(
 			this.height,
 			this.paddleWidth,
 			this.paddleHeight,
 			this.boardGap,
-			((this.height - this.paddleHeight) / 2)
+			((this.height - this.paddleHeight) / 2),
+			KEYS.a, 
+			KEYS.z
 		);
+
+		this.player2 = new Paddle(
+			this.height,
+			this.paddleWidth,
+			this.paddleHeight,
+			(this.width - this.boardGap - this.paddleWidth),
+			((this.height - this.paddleHeight) / 2),
+			KEYS.up, 
+			KEYS.down
+		);
+
+		this.ball = new Ball (8, this.width,	this.height);
 	}
 
 	render() {
@@ -34,6 +51,10 @@ svg.setAttributeNS(null, 'viewBox', `0 0 ${this.width} ${this.height}`);
 
 this.board.render(svg);
 
+this.player1.render(svg);
+this.player2.render(svg);
+
+this.ball.render(svg, this.player1, this.player2);
 
 this.gameElement.appendChild(svg);
 	}
--- src/partials/Paddle.js
+++ src/partials/Paddle.js
@@ -1,28 +1,47 @@
 import { SVG_NS } from '../settings';
 
 export default class Paddle {
-  constructor(boardHeight, width, height, x, y, color, up, down) {
+  constructor(boardHeight, width, height, x, y, up, down) {
     this.boardHeight = boardHeight;
     this.width = width;
     this.height = height;
     this.x = x;
     this.y = y;
-    this.speed = 10;
+    this.speed = 20;
     this.score = 0;
-    this.color = color;
-    this.up = up;
-    this.down = down;
+    // this.color = color;
+    document.addEventListener('keydown', event => {
+      switch (event.key) {
+        case up:   
+          this.up();
+          break;
+        case down:
+          this.down();
+          break;
+      }
+    });
 
+  }//constructor
 
+  up(){
+this.y = Math.max( 0,  this.y - this.speed );
   }
-  //...
+
+  down(){
+this.y = Math.min( this.boardHeight - this.height,   this.y + this.speed); //returns smallest of the two
+  }
+
+  /**
+   * render svg images (block comments)
+   */
+  
   render(svg) {
     let rect = document.createElementNS(SVG_NS, 'rect');
-    rect.setAttributesNS(null, 'fill', 'white');
-    rect.setAttributesNS(null, 'width', this.width);
-    rect.setAttributesNS(null, 'height', this.height);
-    rect.setAttributesNS(null, 'x', this.x);
-    rect.setAttributesNS(null, 'y', this.y);
+    rect.setAttributeNS(null, 'fill', 'white');
+    rect.setAttributeNS(null, 'width', this.width);
+    rect.setAttributeNS(null, 'height', this.height);
+    rect.setAttributeNS(null, 'x', this.x);
+    rect.setAttributeNS(null, 'y', this.y);
 
     svg.appendChild(rect);
   }
